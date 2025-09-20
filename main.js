21bd6621a7266a489e7144b9d7ea86df7b69fd6e
Sumit2004-web <sumitrajera04@gmail.com>
2025-09-20 12:06:27 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,28 @@
+let form=document.querySelector('.form')
+let input=document.querySelector('input')
+let save=document.querySelector('button')
+let list=document.querySelector('.list-sect')
+const savetask=(e)=>{
+      e.preventDefault(); 
+   let li=document.createElement('li');
+   li.className='gp';
+   li.innerText=input.value;
+   let bt1=document.createElement('button');
+ bt1.innerText="Delete";
+ bt1.className="dt";
+   li.appendChild(bt1);
+   list.appendChild(li);
+   console.log(li)
+   form.reset();
+}
+ const removeTodo=(e)=>{
+   if(e.target.className=="dt")
+   {
+      if(confirm("are you sure "))
+       list.removeChild(e.target.parentElement);
+   }
+ }
+list.addEventListener('click',removeTodo)
+form.addEventListener('submit',savetask);
+
+
